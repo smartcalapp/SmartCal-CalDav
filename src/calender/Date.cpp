06d1d17e9414df9fbddae2792947451b7c030a87
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-08-31 22:11:31 -0500

    most of date class just missing epoch -> date

--- src/calender/Date.cpp
+++ src/calender/Date.cpp
@@ -3,3 +3,46 @@
 //
 
 #include "Date.h"
+
+Date epochTime2Date(uint_fast64_t epoch){
+
+}
+
+bool isLeapYear(uint_fast16_t year)
+{
+	if (year % 4 != 0) {
+		return false;
+	} else if (year % 100 != 0) {
+		return true;
+	} else if (year % 400 != 400) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+bool isValidDate(uint_fast16_t year, uint_fast8_t day, uint_fast8_t month)
+{
+	if (day < 1 || month < 1 || month > 12 || year < 1) {
+		return false;
+	}
+	if (month == 2 && isLeapYear(year)) {
+		return day <= 29;
+	}
+	if (month == 2 && !isLeapYear(year)) {
+		return day <= 28;
+	}
+	if (month <= 7 && month % 2 == 1) {
+		return day <= 31;
+	}
+	if (month <= 7 && month % 2 == 0) {
+		return day <= 30;
+	}
+	if (month > 7 && month % 2 == 1) {
+		return day <= 30;
+	}
+	if (month > 7 && month % 2 == 0) {
+		return day <= 31;
+	}
+	return false;
+}
--- src/calender/Date.h
+++ src/calender/Date.h
@@ -6,10 +6,76 @@
 #define SMARTCAL_CALDAV_DATE_H
 
 
+#include <cstdint>
+
+bool isValidDate(uint_fast16_t year, uint_fast8_t day, uint_fast8_t month);
+
 class Date
 {
+	private:
+		uint_fast16_t _year;
+		uint_fast8_t _day;
+		uint_fast8_t _month;
+	public:
+		Date()
+		{
 
+		}
+
+		Date(uint_fast16_t year, uint_fast8_t day, uint_fast8_t month)
+		{
+			if (isValidDate(year, day, month)) {
+				_year = year;
+				_day = day;
+				_month = month;
+			} else {
+				//TODO exept
+			}
+		}
+
+		uint_fast16_t getyear() const
+		{
+			return _year;
+		}
+
+		void setyear(uint_fast16_t year)
+		{
+			if (isValidDate(year, _day, _month)) {
+				Date::_year = year;
+			} else {
+				//TODO exept
+			}
+		}
+
+		uint_fast8_t getday() const
+		{
+			return _day;
+		}
+
+		void setday(uint_fast8_t day)
+		{
+			if (isValidDate(_year, day, _month)) {
+				Date::_day = day;
+			} else {
+				//TODO exept
+			}
+		}
+
+		uint_fast8_t getmonth() const
+		{
+			return _month;
+		}
+
+		void setmonth(uint_fast8_t month)
+		{
+			if (isValidDate(_year, _day, month)) {
+				Date::_month = month;
+			} else {
+				//TODO exept
+			}
+		}
 };
 
+Date epochTime2Date(uint_fast64_t epoch);
 
 #endif //SMARTCAL_CALDAV_DATE_H
