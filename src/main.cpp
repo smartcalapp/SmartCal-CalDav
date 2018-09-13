e457068cb34358dc2af8994de46a11a1658f9867
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-09-13 14:27:08 -0500

    fixed issue #11

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,16 @@
 #include "net/socket.h"
 #include "config/config.h"
 
+void signalHandler( int signum ) {
+	(void) signum;
+	printf("clearing child");
+	fflush(stdout);
+	wait(NULL);
+}
 
 int main(int argc, char **argv) {
+	signal(SIGCHLD, signalHandler);
+
 	if (argc != 2) {
 		std::cout << "please only specify path to config file" << std::endl;
 		exit(-1);
