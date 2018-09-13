1aafe58c7c2f7b6d49f2ac1585db7a979eff0fb6
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-09-13 14:34:10 -0500

    version bump WELCOME TO MVP

--- src/calender/iCalDefs.h
+++ src/calender/iCalDefs.h
@@ -7,7 +7,7 @@
 
 //TODO read from file? or something?
 #define PROD_ID "SMARTCAL-WEBDAV"
-#define VERSION_ID "0.1"
+#define VERSION_ID "1.0-MVP"
 #define ICAL_LINE_SEP "\r\n"
 
 #endif //SMARTCAL_CALDAV_ICALDEFS_H
